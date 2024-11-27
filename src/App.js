bdde7754302dd2e374d09d016cb434ca4b9b54cf
Nguyenhoang164 <kamito062004@gmail.com>
2024-11-27 18:04:51 +0700

    done base fe home and save csrf to login

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './App/services/IAM/Login';
 import Register from './App/services/IAM/Register';
-import Home from './App/services/Home';
 import ChangePassword from "./App/services/IAM/ChangePassword";
+import HomeAdmin from './App/services/Business/HomePage/HomeAdmin';
 
 function App() {
   return(
@@ -13,7 +13,7 @@ function App() {
  <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/register' element={<Register/>} />
-      <Route path='/home' element={<Home/>}/>
+      <Route path='/homeAdmin' element={<HomeAdmin/>}/>
       <Route path='/change-password' element={<ChangePassword/>}/>
 </Routes>
 </>
