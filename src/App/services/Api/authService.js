9dbd18318364225f9f81a932e7405e68c5da8b2d
Trần Đức Thịnh <thinhtran9797yb@gmail.com>
2024-12-05 17:32:06 +0700

    add cmt code

--- src/App/services/Api/authService.js
+++ src/App/services/Api/authService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:8888/api/v1";
 
+// api register
 export const register = async (data) => {
     return axios.post(`${API_BASE_URL}/auth/register`, data);
 };
